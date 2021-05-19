a521b4848d643f582453e3d7a5bb5b8e69bdbb1e
Westley Feller <feller.westley@gmail.com>
2021-05-19 11:52:15 -0700

    chore: setup project tools
    Linting, formatting, error logging, semantic release all configured

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import "../styles/globals.css"
+import { AppProps } from "next/app"
+import { FC } from "react"
+import { init } from "@sentry/nextjs"
+
+init({
+	dsn: "https://a7cf5bef4a6f4bd396e719be490aff3b@o481108.ingest.sentry.io/5774364",
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+})
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />
+}
+
+export default MyApp
