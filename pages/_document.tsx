b9f10ee0f7ebe0cd155cd3279b93183b86835d68
Westley Feller <feller.westley@gmail.com>
2021-05-19 22:55:29 -0700

    style: formatting with prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,38 @@
 import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
 } from "next/document"
 import { ColorModeScript } from "@chakra-ui/react"
 import React, { ReactElement, ReactFragment } from "react"
 
 class MyDocument extends Document {
-	static async getInitialProps(ctx: unknown): Promise<{
-		head?: Array<JSX.Element | null>
-		html: string
-		styles?: ReactElement[] | ReactFragment
-	}> {
-		const initialProps = await Document.getInitialProps(ctx as DocumentContext)
-		return { ...initialProps }
-	}
+  static async getInitialProps(ctx: unknown): Promise<{
+    head?: Array<JSX.Element | null>
+    html: string
+    styles?: ReactElement[] | ReactFragment
+  }> {
+    const initialProps = await Document.getInitialProps(ctx as DocumentContext)
+    return { ...initialProps }
+  }
 
-	render(): JSX.Element {
-		return (
-			<Html lang="en">
-				<Head>
-					<link rel="icon" href="/favicon.ico" />
-				</Head>
-				<body>
-					<ColorModeScript initialColorMode="light" />
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
-	}
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode="light" />
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
